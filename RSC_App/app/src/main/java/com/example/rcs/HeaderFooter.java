070289cff86a71647ee14be0e70415eed849ca10
MKQ <khuatquang1807@gmail.com>
2024-10-19 19:40:46 +0700

    hf

--- RSC_App/app/src/main/java/com/example/rcs/HeaderFooter.java
+++ RSC_App/app/src/main/java/com/example/rcs/HeaderFooter.java
@@ -1,10 +1,8 @@
 package com.example.rcs;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,32 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class HeaderFooter extends AppCompatActivity {
 
-    Button btnT;
-
-
+    Toolbar header;
+    ImageView logo;
+    ImageView bell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_footer);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btnT = findViewById(R.id.btnT);
-        btnT.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HeaderFooter.class);
-                startActivity(intent);
-            }
-        });
+        header = findViewById(R.id.Header);
 
-
+        header.setElevation(4);
     }
 }
